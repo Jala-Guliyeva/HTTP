eab68b167a216870757caeabbaf058e930545dd5
Jala Guliyeva <tu7knjupn@aztu.local>
2022-04-08 00:51:12 -0800

    file added

---
+++ Soonn Task/Soonn Task/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soonn_Task.Models
+{
+    internal class Student
+    {
+        public int No { get; set; }
+        public string FullName { get; set; }
+
+        Dictionary<string, double> Exams;
+
+        public Student()
+        {
+            Exams = new Dictionary<string, double>();
+
+        }
+
+        public void AddExam(string examName,double point)
+        {
+            if (examName==null )
+            {
+                throw new NullReferenceException("name null ola bilmez");
+            }
+            Exams.Add(examName, point);
+
+
+        }
+        public void GetExamResult(string examName)
+        {
+            if (examName == null)
+            {
+                throw new NullReferenceException("name null ola bilmez");
+            }
+            Exams.ContainsValue(1);
+          
+        }   
+        public void GetExamAvg()
+        {
+            if (Exams == null)
+            {
+                throw new NullReferenceException("Exams null ola bilmez");
+            }
+            
+           
+        }
+    
+        
+        
+       
+       
+
+
+
+    }
+}
---
+++ Soonn Task/Soonn Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using Soonn_Task.Models;
+using System;
+
+namespace Soonn_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            bool check = true;
+
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1.  Telebe elave et");
+                Console.WriteLine("2. Telebeye imtahan elave et");
+                Console.WriteLine("3. Telebenin bir imtahan balina bax");
+                Console.WriteLine("4. Telebenin butun imtahanlarini goster");
+                Console.WriteLine("5. Telebenin imtahan ortalamasini goster");
+                Console.WriteLine("6. Telebeden imtahan sil");
+                Console.WriteLine("0. Quit");
+                string choice = Console.ReadLine();
+                switch (check)
+                { case true:    
+                    
+                }break;
+
+            } while (true);
+
+
+
+        }    
+    }
+}
